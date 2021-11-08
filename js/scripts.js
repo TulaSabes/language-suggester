b5fced16a027b54d652d62ce8e749a0500891e63
TulaSabes <sabestula14@gmail.com>
2021-11-08 15:53:43 -0800

    Making the submit button give a submission alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// Moose
+// Languages
 $(document).ready(function() {
     $("#choose-language").change(function() {
       const language = $("#choose-language").val();
@@ -17,7 +17,19 @@ $(document).ready(function() {
       } else if (language === "Rust") {
         $("#Rust").show();
       }
-  
+//Submits Survey and gives submission messege
     });
+    $("#message-alert").submit(function(e){
+      const form = e.target;
+      const formSumbission= $("#messege-alert").serialiaze();
+      debugger;
+      // const inputs= Array.from(form.querySelectorAll('input')).filter((item)=> item.checked).map((item) => { return `${item.name}: ${item.value}`;  });
+      console.log(inputs)
+      alert("Your survey has been submitted successfully!")
+      
+      
+
+
+    })
   });
   
